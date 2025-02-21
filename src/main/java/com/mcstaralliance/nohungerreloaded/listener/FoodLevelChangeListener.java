e2a4110bd2bce03fee1cdb4d8eb7433b9cdc0aea
LDS_XiaoYe <xiaoyecreate@outlook.com>
2025-02-21 19:55:55 +0800

    新增 HumanEntity 强转 Player 时检测

--- src/main/java/com/mcstaralliance/nohungerreloaded/listener/FoodLevelChangeListener.java
+++ src/main/java/com/mcstaralliance/nohungerreloaded/listener/FoodLevelChangeListener.java
@@ -14,6 +14,9 @@ public class FoodLevelChangeListener implements Listener {
     public void onFoodLevelChange(FoodLevelChangeEvent event) {
         // FoodLevelChangeEvent 无法直接获取到 Player 对象，故使用 HumanEntity 转换
         HumanEntity entity = event.getEntity();
+        if (!(entity instanceof Player)) {
+            return;
+        }
         Player player = (Player) entity;
         String name = player.getName();
         if (!ConfigManager.getNoHungerList().contains(name)) {
