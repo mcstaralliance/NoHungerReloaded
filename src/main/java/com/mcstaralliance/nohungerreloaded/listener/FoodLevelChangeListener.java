2bce61f50bdfaacc5b3fd784d21c01d39ad5635c
LDS_XiaoYe <xiaoyecreate@outlook.com>
2025-02-21 19:51:58 +0800

    尝试修复消耗饱食度回血时，不会自动补充饱食度的问题

--- src/main/java/com/mcstaralliance/nohungerreloaded/listener/FoodLevelChangeListener.java
+++ src/main/java/com/mcstaralliance/nohungerreloaded/listener/FoodLevelChangeListener.java
@@ -2,6 +2,7 @@ package com.mcstaralliance.nohungerreloaded.listener;
 
 import com.mcstaralliance.nohungerreloaded.manager.ConfigManager;
 import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
@@ -11,15 +12,13 @@ public class FoodLevelChangeListener implements Listener {
 
     @EventHandler
     public void onFoodLevelChange(FoodLevelChangeEvent event) {
-        // if nohunger is on, player can eat foods.
-        if (event.getItem() != null) {
-            return;
-        }
+        // FoodLevelChangeEvent 无法直接获取到 Player 对象，故使用 HumanEntity 转换
         HumanEntity entity = event.getEntity();
-        String name = entity.getName();
+        Player player = (Player) entity;
+        String name = player.getName();
         if (!ConfigManager.getNoHungerList().contains(name)) {
             return;
         }
-        entity.setFoodLevel(20);
+        player.setFoodLevel(20);
     }
 }
